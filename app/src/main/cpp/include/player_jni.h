2f59a1d67ae61007f174338d52abda27a03e9021
thinkreed <thinkreed2017@outlook.com>
2017-09-04 22:59:22 +0800

    add basic ffplay

---
+++ app/src/main/cpp/include/player_jni.h
@@ -0,0 +1,8 @@
+//
+// Created by thinkreed on 2017/9/4.
+//
+
+#ifndef RFPLAYER_PLAYER_JNI_H_H
+#define RFPLAYER_PLAYER_JNI_H_H
+int open_ffmpeg();
+#endif //RFPLAYER_PLAYER_JNI_H_H
--- app/src/main/cpp/player_jni.c
+++ app/src/main/cpp/player_jni.c
@@ -1,13 +1,85 @@
 #include <jni.h>
 
 #include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "player_jni.h"
 
 JNIEXPORT jstring JNICALL
 Java_think_reed_rfplayer_MainActivity_stringFromFFmpeg(JNIEnv *env, jobject instance) {
 
     // TODO
     char info[10000] = {0};
-    sprintf(info, "%s\n", avcodec_configuration());
+    sprintf(info, "the result is %d\n", open_ffmpeg());
 
     return (*env)->NewStringUTF(env, info);
+}
+
+int open_ffmpeg() {
+
+    AVFormatContext *pFormatCtx = NULL;
+    int i, videoStream;
+    AVCodecParameters *pCodecParam;
+    AVCodecContext *pCodecCtx;
+    AVCodec *pCodec;
+    AVFrame *pFrame;
+    AVFrame *pFrameYUV;
+    AVPacket *packet;
+    int frameFinished;
+    int numBytes;
+    uint8_t *buffer;
+
+    char filePath[] = "/sdcard/video/dcw.mp4";
+
+    //register all codecs
+    av_register_all();
+
+    //init network module
+    avformat_network_init();
+
+    pFormatCtx = avformat_alloc_context();
+
+    if (avformat_open_input(pFormatCtx, filePath, NULL, NULL) != 0) {
+        return -1;
+    }
+
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+        return -1;
+    }
+
+    videoStream = -1;
+
+    for (i = 0; i < pFormatCtx->nb_streams; i++) {
+        if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = i;
+            break;
+        }
+    }
+
+    if (videoStream == -1) {
+        return -1;
+    }
+
+    pCodecParam = pFormatCtx->streams[videoStream]->codecpar;
+
+    pCodec = avcodec_find_decoder(pCodecParam->codec_id);
+
+    pCodecCtx = avcodec_alloc_context3(pCodec);
+
+    if (pCodec == NULL) {
+        return -1;
+    }
+
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+        return -1;
+    }
+
+    pFrame = av_frame_alloc();
+
+    pFrameYUV = av_frame_alloc();
+
+    packet = (AVPacket*) av_malloc(sizeof(AVPacket));
+
+
+
+    return 0;
 }
